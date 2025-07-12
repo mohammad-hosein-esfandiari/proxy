3c7700e23d86f291b6fcd3bd632392bfecd8ff92
Mohammad <mohammadhosseinesfandiari1@gmail.com>
2025-07-12 21:57:28 +0330

    test vercel 2

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const serverless = require("serverless-http");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const proxyRoutes = require("./routes/my-vocab-app-proxy");
+app.use("/api", proxyRoutes);
+
+// ❌ نباید app.listen() داشته باشی
+// module.exports.handler = serverless(app);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Proxy server is running on port http://localhost:${PORT}`);
+}); 
